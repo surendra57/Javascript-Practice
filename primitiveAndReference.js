cb4358d48bcae0fee6b720e39e2bca4cb105e029
Surendra Gahlot <surendragahlot57@gmail.com>
2023-08-26 21:31:54 +0530

    reference data

---
+++ primitiveAndReference.js
@@ -0,0 +1,28 @@
+// Primitive and reference data type
+
+//primitive data type that have fixed value and size in memory and reference data type  does not have fix size in memory
+
+
+// primitive data type example
+
+var a = 10
+var b = a
+
+var b = b*2
+
+/* Non primitive data type (Reference data type )
+ {}()[] all brackets are reference data types otherwise are primitive data type 
+like ----number.
+bigint.
+boolean.
+undefined.
+symbol.
+null. */
+
+var arr1 = [1,23,8]
+var arr2 = arr1
+
+
+arr2.push(2)
+
+// changes are made in arr1 also 
