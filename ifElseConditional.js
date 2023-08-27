a78522056719b67d32905a3cc7ebf2e62790dce3
Surendra Gahlot <surendragahlot57@gmail.com>
2023-08-27 18:12:52 +0530

    truthy and falsy value

---
+++ ifElseConditional.js
@@ -0,0 +1,21 @@
+// if else and else if conditonal operators
+
+// also we check the truthy and falsy value
+
+if(null){
+    console.log('if')
+}else{
+    console.log('else')
+}
+
+
+// all values are truthy valuse expect these value 
+/* 
+null 
+undefined
+NaN
+-0
+0n
+""
+false
+*/
