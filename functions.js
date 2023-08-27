704c0d382adfddd4983cb372095e9f6d6b3ab9f9
Surendra Gahlot <surendragahlot57@gmail.com>
2023-08-27 20:52:47 +0530

    functions

---
+++ functions.js
@@ -0,0 +1,11 @@
+// Functions
+// 1) Its a block of code that desgined to perform a particular many times.
+
+// 2) that is defines once and excute and use it many times where we want to use.
+
+function first(){
+    console.log("hello bolo")
+}
+
+first()
+
---
+++ loops.js
@@ -0,0 +1,25 @@
+// for and while loop
+
+// for loop
+// print 0-10 numbers
+for(let i=0;i<20;i++){
+    console.log(i)
+}
+// print 25-50 numbers
+// for(var i =25;i<51;i++){
+//     console.log(i)
+// }
+
+// Now using While loop
+
+// var i=20;
+// while(i<31){
+// console.log(i)
+// i++;
+// }
+
+var i=20
+do {
+    console.log(i)
+    i++;   
+} while (i<31);
