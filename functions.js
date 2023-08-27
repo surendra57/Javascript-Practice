1254c24a7219eb9c176833e343180c10c3d267a6
Surendra Gahlot <surendragahlot57@gmail.com>
2023-08-27 21:57:48 +0530

    functions

--- functions.js
+++ functions.js
@@ -3,9 +3,35 @@
 
 // 2) that is defines once and excute and use it many times where we want to use.
 
-function first(){
+function first(ele){
     console.log("hello bolo")
+    return ele+2
 }
 
-first()
+console.log(first())
+
+//  check even numbers 
+
+function isEven(num){
+
+    if(num%2 ===0){
+        return true
+    }else{
+        return false
+    }
+
+}
+
+console.log(isEven(10))
+
+// input string and output first character of string
+
+function firstChar(str){
+
+    // return str[0]
+    return str[str.length-1]
+
+}
+console.log(firstChar('Surendra'))
+
 
