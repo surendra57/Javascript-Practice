02d7c5a028dc057e8a2f3504bf812061066ed3f5
Surendra Gahlot <surendragahlot57@gmail.com>
2023-08-30 16:09:06 +0530

    execution context

---
+++ executionContext.js
@@ -0,0 +1,23 @@
+// Execution context 
+// it handles the entire transformation and execution of code
+
+function abcd(){
+
+    var a =10;
+    function def(){
+        var b = 12;
+
+    }
+    
+}
+function two(){
+    console.log(a);
+  }
+  function one(){
+    var a=2;
+    console.log(a);
+    two();
+  }
+  var a=1;
+  console.log(a);
+  one();
